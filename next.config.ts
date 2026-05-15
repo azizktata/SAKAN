10fca1f31d334628bc243c45991a3f341a8fa595
unknown <azizktata>
2026-05-15 11:14:03 +0100

    add images script

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,14 @@ const nextConfig: NextConfig = {
     ]
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'cdn.sakan.tn' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'http',  hostname: 'localhost', port: '8000' },
+      { protocol: 'https', hostname: 'www.sakan.marinekeys.com' },
+      { protocol: 'https', hostname: 'sakan.marinekeys.com' },
+    ],
   },
 }
 
